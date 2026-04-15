d9ef558f14fffb134b3a1c68830e9d30f3a007aa
ShubhamDubey05 <shubhamdubey05092001@gmail.com>
2026-04-15 17:22:47 +0530

    db name change

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,17 +1,16 @@
-const mongoose = require("mongoose")
-
-
+const mongoose = require("mongoose");
+const { DB_NAME } = require("../constants.js");
 
 async function connectToDB() {
-
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI, {
+            dbName: DB_NAME
+        });
 
-        console.log("Connected to Database")
-    }
-    catch (err) {
-        console.log(err)
+        console.log("Connected to Database");
+    } catch (err) {
+        console.log("DB Error:", err);
     }
 }
 
-module.exports = connectToDB
+module.exports = connectToDB;
